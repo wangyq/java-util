281616aee7dc0d3a117651cfe302ef8d64b42118
yinqingwang <yinqingwang@gmail.com>
2012-11-29 16:12:18 +0800

    add echo server

---
+++ src/com/firestudio/server/EchoProtocol.java
@@ -0,0 +1,144 @@
+package com.firestudio.server;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.nio.CharBuffer;
+import java.nio.channels.SelectionKey;
+import java.nio.channels.ServerSocketChannel;
+import java.nio.channels.SocketChannel;
+import java.nio.charset.Charset;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.CharsetEncoder;
+
+import com.sun.org.apache.bcel.internal.generic.NEW;
+
+public class EchoProtocol implements Protocolable {
+
+	/**
+	 * 
+	 */
+	private int buf_size = 1024;
+	/**
+	 * 
+	 */
+	private ByteBuffer readBuffer = null;
+	/**
+	 * 
+	 */
+	private ByteBuffer writeBuffer = null;
+
+	/**
+	 * 
+	 */
+	StringBuffer stringBuffer = new StringBuffer("");
+	/**
+	 * 
+	 */
+	CharsetEncoder encoder = null;
+
+	/**
+	 * 
+	 */
+	CharsetDecoder decoder = null;
+
+	/**
+	 * Construction
+	 */
+	public EchoProtocol() {
+		readBuffer = ByteBuffer.allocate(buf_size);
+		writeBuffer = ByteBuffer.allocate(buf_size);
+
+		Charset charset = Charset.forName("UTF-8");
+		encoder = charset.newEncoder();
+		decoder = charset.newDecoder();
+
+	}
+
+	@Override
+	public void handleAccept(SelectionKey key) throws IOException {
+		// TODO Auto-generated method stub
+		SocketChannel clntChan = ((ServerSocketChannel) key.channel()).accept();
+
+		System.out.println("Recieve a client connection from "
+				+ clntChan.socket().getRemoteSocketAddress().toString());
+
+		clntChan.configureBlocking(false); // Must be nonblocking to register
+		// Register the selector with new channel for read and attach byte
+		// buffer
+		// clntChan.register(key.selector(), SelectionKey.OP_READ,
+		// ByteBuffer.allocate(bufSize));
+		// CharBuffer buffer =
+		// CharBuffer.wrap("Welcome to Echo Server.  Type \"Quit\" to quit the server. \r\n");
+		String str = "Welcome to Echo Server.  Type \"Quit\" to quit the server. \r\n";
+		writeBuffer.put(str.getBytes());
+
+		clntChan.register(key.selector(), SelectionKey.OP_WRITE, this);
+	}
+
+	@Override
+	public void handleRead(SelectionKey key) throws IOException {
+		// TODO Auto-generated method stub
+
+		SocketChannel clntChan = (SocketChannel) key.channel();
+
+		// decoder.reset();// clearing any internal state
+		boolean bExit = false;
+		int Ops = SelectionKey.OP_READ;
+
+		// while (true) { // repeat read until buffer is empty!
+		readBuffer.clear();
+		int bytesRead = clntChan.read(readBuffer);
+		if (bytesRead == -1) { // Did the other end close?
+			bExit = true;
+			// clntChan.close();
+			// System.out.println("Client close connection!");
+			// break;
+		} else if (bytesRead == 0) {
+			System.out.println("Read Buffer is full!");
+		} else {
+			// else if (bytesRead > 0) {
+			// Indicate via key that reading/writing are both of interest
+			// now.
+			// CharBuffer cbuf = decoder.decode(readBuffer);
+			String str = new String(readBuffer.array(), 0, bytesRead);
+			stringBuffer.append(str);
+
+			// key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+
+			// }//end of while read
+			if (stringBuffer.lastIndexOf("\n") != -1) {
+				System.out.print(stringBuffer);
+				// CharBuffer buffer = CharBuffer.wrap(stringBuffer.toString());
+				// writeBuffer.put(encoder.encode(buffer));
+				writeBuffer.put(stringBuffer.toString().getBytes());
+				Ops |= SelectionKey.OP_WRITE;
+
+				stringBuffer.setLength(0);
+			}
+		}
+		 //System.out.println(stringBuffer);
+		if (!bExit) {
+			// key.interestOps(SelectionKey.OP_READ | SelectionKey.OP_WRITE);
+			key.interestOps(Ops);
+		} else {
+			clntChan.close();
+			System.out.println("Client close connection!");
+		}
+	}
+
+	@Override
+	public void handleWrite(SelectionKey key) throws IOException {
+		// TODO Auto-generated method stub
+		SocketChannel clntChan = (SocketChannel) key.channel();
+
+		writeBuffer.flip(); // Prepare buffer for writing
+		clntChan.write(writeBuffer);
+
+		if (!writeBuffer.hasRemaining()) { // Buffer completely written?
+			// Nothing left, so no longer interested in writes
+			key.interestOps(SelectionKey.OP_READ);
+		}
+		writeBuffer.compact(); // Make room for more data to be read in
+	}
+
+}
---
+++ src/com/firestudio/server/EchoTcpServer.java
@@ -0,0 +1,82 @@
+package com.firestudio.server;
+
+import java.net.*;  // for Socket, ServerSocket, and InetAddress
+import java.nio.channels.*;
+import java.util.Iterator;
+import java.io.*;   // for IOException and Input/OutputStream
+
+/**
+ * Tcp Echo Server implementation using nio 
+ * @author wangyq
+ *
+ */
+public class EchoTcpServer {
+
+	/**
+	 * Server wait time out 
+	 */
+	private static final int TIMEOUT = 3000; // Wait timeout (milliseconds)
+	
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args)  throws IOException{
+		// TODO Auto-generated method stub
+		int echoServPort = 23;  //default port number
+	    if (args.length >= 1) { // Test for correct # of args
+	    	echoServPort = Integer.parseInt(args[0]); // Server port
+	        //throw new IllegalArgumentException("Parameter(s): <Port>");
+	      }
+
+	      //echoServPort = Integer.parseInt(args[0]); // Server port
+	      
+	      // Create a selector to multiplex listening sockets and connections
+	      Selector selector = Selector.open();
+	      
+	      ServerSocketChannel listnChannel = ServerSocketChannel.open();
+	      listnChannel.socket().bind(new InetSocketAddress(echoServPort));
+	      listnChannel.configureBlocking(false); // must be nonblocking to register
+	      // Register selector with channel. The returned key is ignored
+	      listnChannel.register(selector, SelectionKey.OP_ACCEPT);
+	      
+	      System.out.println("Server get ready at port : " + echoServPort);
+	      
+	      while(true){// Run forever, processing available I/O operations
+	          // Wait for some channel to be ready (or timeout)
+	          if (selector.select(TIMEOUT) == 0) { // returns # of ready chans
+	              //System.out.print(".");
+	              continue;
+	            }
+
+	            // Get iterator on set of keys with I/O to process
+	            Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
+	            
+	            while (keyIter.hasNext()) { //process all key with I/O already
+	              SelectionKey key = keyIter.next(); // Key is bit mask
+	              // Server socket channel has pending connection requests?
+	              if (key.isAcceptable()) {
+	                //protocol.handleAccept(key);
+	            	//  handleAccept(key);
+	            	  new EchoProtocol().handleAccept(key);
+	            	  
+	              }
+	              // Client socket channel has pending data?
+	              if (key.isReadable()) {
+	                //protocol.handleRead(key);
+	            	  ((Protocolable)key.attachment()).handleRead(key);
+	              }
+	              // Client socket channel is available for writing and
+	              // key is valid (i.e., channel not closed)?
+	              if (key.isValid() && key.isWritable()) {
+	                //protocol.handleWrite(key);
+	            	  ((Protocolable)key.attachment()).handleWrite(key);
+	              }
+	              keyIter.remove(); // remove from set of selected keys
+	            }
+	    	  
+	      }// end of while(true)
+	      //not reachable!
+	      
+	}
+	
+}
